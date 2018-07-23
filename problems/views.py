be7e47cc6892c9d047be1516e6757d69a55caa1d
Paul Jickling <pjickling@gmail.com>
2018-07-23 08:38:10 -0700

    Added randomly selected problem to view template

--- problems/views.py
+++ problems/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import Context, Template
+from django.template.loader import get_template
+from .models import Problem
+import random
 
 def index(request):
-	return render(request, 'questions/index.html',)
+	problems = Problem.objects.all()
+	random_problem = random.choice(problems)
+	title = "Project Title"
+	context = {'random_problem': random_problem,}
+
+	return render(request, 'questions/index.html', context)
