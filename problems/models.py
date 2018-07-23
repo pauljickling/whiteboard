5e80c0a65adf38b76471b299cb148bf33b3fdcbf
Paul Jickling <pjickling@gmail.com>
2018-07-22 23:40:46 -0700

    Preliminary batch of questions added

--- problems/models.py
+++ problems/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+NON_CODING = "Non_Coding"
+ARRAYS_AND_STRINGS = "Arrays and Strings"
+LINKED_LISTS = "Linked Lists"
+STACKS_AND_QUEUES = "Stacks and Queues"
+TREES_AND_GRAPHS = "Trees and Graphs"
+BIT_MANIPULATION = "Bit Manipulation"
+MATH_AND_LOGIC_PUZZLES = "Math and Logic Puzzles"
+OBJECT_ORIENTED_DESIGN = "Object Oriented Design"
+RECURSION_AND_DYNAMIC_PROGRAMMING = "Recursion and Dynamic Programming"
+SYSTEM_DESIGN_AND_SCALABILITY = "System Design and Scalability"
+SORTING_AND_SEARCHING = "Sorting and Searching"
+TESTING = "Testing"
+
+CATEGORY_CHOICES = (
+	(NON_CODING, "Non-Coding"),
+	(ARRAYS_AND_STRINGS, 'Arrays and Strings'),
+	(LINKED_LISTS, 'Linked Lists'),
+	(STACKS_AND_QUEUES, 'Stacks and Queues'),
+	(TREES_AND_GRAPHS, 'Trees and Graphs'),
+	(BIT_MANIPULATION, 'Bit Manipulation'),
+	(MATH_AND_LOGIC_PUZZLES, 'Math and Logic Puzzles'),
+	(OBJECT_ORIENTED_DESIGN, 'Object Oriented Design'),
+	(RECURSION_AND_DYNAMIC_PROGRAMMING, 'Recursion and Dynamic Programming'),
+	(SYSTEM_DESIGN_AND_SCALABILITY, 'System Design and Scalability'),
+	(SORTING_AND_SEARCHING, 'Sorting and Searching'),
+	(TESTING, 'Testing')
+)
+
+
+class Question(models.Model):
+	question = models.TextField()
+	starred = models.BooleanField(default=False)
+	category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default=NON_CODING)
+
+	def __str__(self):
+		return self.question
+
+PSEUDOCODE = "PC"
+CPLUSPLUS = "CP"
+GO = "GO"
+JAVA = "JV"
+JAVASCRIPT = "JS"
+PYTHON = "PY"
+RUBY = "RB"
+RUST = "RS"
+
+LANGUAGE_CHOICES = (
+	(PSEUDOCODE, 'Psuedocode and/or Writing'),
+	(CPLUSPLUS, 'C++'),
+	(GO, 'Go'),
+	(JAVA, 'Java'),
+	(JAVASCRIPT, 'Javascript'),
+	(PYTHON, 'Python'),
+	(RUBY, 'Ruby'),
+	(RUST, 'Rust')
+)
+
+class Answer(models.Model):
+	answer = models.TextField()
+	language = models.CharField(max_length=30, choices = LANGUAGE_CHOICES, default=JAVASCRIPT)
+	correct = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.answer
--- whiteboard/urls.py
+++ whiteboard/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf.urls import url
 
 urlpatterns = [
+	path('', include('problems.urls')),
     path('admin/', admin.site.urls),
 ]
