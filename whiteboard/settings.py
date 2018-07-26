90b8bfc599f268c2631e7057154d52204508f446
Paul Jickling <pjickling@gmail.com>
2018-07-26 11:29:43 -0700

    Switched database to postgresql

--- whiteboard/settings.py
+++ whiteboard/settings.py
@@ -79,8 +79,9 @@ WSGI_APPLICATION = 'whiteboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'paulj',
+        'USER': 'paulj',
     }
 }
 
