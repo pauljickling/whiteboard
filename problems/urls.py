6a08943c197eb0817e52824b9f802ba4bc9c6388
Paul Jickling <pjickling@gmail.com>
2018-08-20 14:02:44 -0700

    Added category URLs

--- problems/urls.py
+++ problems/urls.py
@@ -4,5 +4,16 @@ from . import views
 
 urlpatterns = [
 path('', views.index, name='problems'),
-#path('/<slug:slug>', views.problem_category, name='problem_category'),
+path('all', views.index, name='problems'),
+path('arrays-and-strings', views.index, name='problems'),
+path('javascript-problems', views.index, name='problems'),
+path('linked-lists', views.index, name='problems'),
+path('math-and-logic-puzzles', views.index, name='problems'),
+path('object-oriented-design', views.index, name='problems'),
+path('recursion-and-dynamic-programming', views.index, name='problems'),
+path('sorting-and-searching', views.index, name='problems'),
+path('stacks-and-queues', views.index, name='problems'),
+path('system-design-and-scalability', views.index, name='problems'),
+path('testing-and-debugging', views.index, name='problems'),
+path('trees-and-graphs', views.index, name='problems'),
 ]
