0e04f32e829c9ff6a52d6f7733bacc87278bccdc
Paul Jickling <pjickling@gmail.com>
2018-10-13 15:12:13 -0700

    Removed unnecessary javascript and broken form section

--- static/category_select.js
+++
@@ -1,8 +0,0 @@
-//let categorySelect = document.querySelector('select#id_category').value;
-let problemType = document.querySelector('p.problem-type').textContent;
-let problem = document.querySelector('p.problem').textContent;
-let problemSelector = document.querySelector('select#id_problem');
-problemSelector.value = problem;
-
-console.log(problemSelector);
-console.log(problem);
